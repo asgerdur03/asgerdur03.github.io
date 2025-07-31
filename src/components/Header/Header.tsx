39b8cf7a38d8b2ad1e8b0b321186c181a816f1de
asgerdur03 <asgerdur03@gmail.com>
2025-07-31 17:42:34 +0000

    for dev

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,8 @@ export default function Header() {
     return <>
     <nav className={styles.nav}>
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>TODO - navbar</li>
+            
         </ul>
         <Image src="/pikachu.svg" alt="inspo pic" width={100} height={60}/>
         
